5e5a3e816a9d86bd1b07efc528903703421468bd
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-08-13 20:27:35 -0300

    fix: correção de linter/prettir localmente + comenta console.log auxiliar no middleware de erro

--- app.js
+++ app.js
@@ -32,5 +32,5 @@ app.use(Middlewares.error);
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
+// você deve usar o arquivo index.js para executar sua aplicação
 module.exports = app;
